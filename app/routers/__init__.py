ad81a1609cda0a91a66cf61b98374702aa35cfa0
aayush598 <aayushgid598@gmail.com>
2025-07-22 18:41:26 +0530

    API endpoint created

--- app/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# app/routers/__init__.py
-"""API route handlers"""
-
-from . import members, projects, matching, algorithm, utils
-
-__all__ = ["members", "projects", "matching", "algorithm", "utils"]
